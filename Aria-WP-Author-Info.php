77a82064259f7335248aac0972d5ea48e4b403b5
aria <shemshad@live.com>
2018-02-17 15:24:09 +0330

    upload in wp fix

--- Aria-WP-Author-Info.php
+++ Aria-WP-Author-Info.php
@@ -38,14 +38,14 @@ function wpb_author_info_box( $content ) {
 
         if ( ! empty( $display_name ) )
 
-            $author_details = '<p style="direction: rtl" class="author_name">درباره نویسنده پست : ' . $display_name . '</p>';
+            $author_details = '<p style="direction: rtl" class="awai_author_name">درباره نویسنده پست : ' . $display_name . '</p>';
 
         if ( ! empty( $user_description ) )
 // Author avatar and bio
 
-            $author_details .= '<p class="author_details">' . get_avatar( get_the_author_meta('user_email') , 90 ) . nl2br( $user_description ). '</p>';
+            $author_details .= '<p class="awai_author_details">' . get_avatar( get_the_author_meta('user_email') , 90 ) . nl2br( $user_description ). '</p>';
 
-        $author_details .= '<p class="author_links"><a href="'. $user_posts .'">نمایش تمام پست های  ' . $display_name . '</a>';
+        $author_details .= '<p class="awai_author_links"><a href="'. $user_posts .'">نمایش تمام پست های  ' . $display_name . '</a>';
 
 // Check if author has a website in their profile
         if ( ! empty( $user_website ) ) {
@@ -59,7 +59,7 @@ function wpb_author_info_box( $content ) {
         }
 
 // Pass all this info to post content
-        $content = $content . '<footer class="author_bio_section" >' . $author_details . '</footer>';
+        $content = $content . '<footer class="awai_author_bio_section" >' . $author_details . '</footer>';
     }
     return $content;
 }
@@ -76,7 +76,7 @@ function add_theme_scripts() {
 
     wp_enqueue_style( 'slider', plugin_dir_url( __FILE__ ) . 'assets/style.css', array(), '1.1', 'all');
 
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+    // wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Conversion of Rial to Toman for Easy Digital Downloads (EDD) ===
+
+Contributors: https://profiles.wordpress.org/ariapadweb
+Plugin Name: Aria WP Author Info
+Plugin URI: https://ariapadweb.ir/aria-wp-author-info-plugin-display-wordpress-writer-information/
+Tags: author,Authorbox
+Author URI: https://ariapadweb.ir/
+Author: shemshad amiri khorasani
+Donate link:
+Requires at least: 4.0
+Tested up to: 4.9.1
+Stable tag: 4.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Display author information at the bottom of the posts
+
+== Description ==
+
+Display author information at the bottom of the posts
+
+Link project on GitHub : https://github.com/arshaparthia/Aria-WP-Author-Info/
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/Aria-WP-Author-Info` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. (Make your instructions match the desired user flow for activating and installing your plugin. Include any steps that might be needed for explanatory purposes)
+
+
+== Frequently Asked Questions ==
+
+= How this plugin works =
+
+Display author information at the bottom of the posts
+
+
+== Screenshots ==
+
+1. screenshot-1.png
+2. screenshot-2.png
+== Changelog ==
+
+= 1.0 =
+* fix a bug
